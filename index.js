bfe3d2f22f7e3527c8193009fe240fc8fe266806
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2020-12-05 18:25:17 +0800

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  const phonebookInfo = `Phonebook has info for ${persons.length} people`
+  response.send(`<!DOCTYPE html>
+
+  <div>
+  <p>${phonebookInfo}</p>
+  <p>${new Date()}</p>
+  </div>
+  `)
+})
+
 app.get('/api/persons', (request, response) => [
   response.json(persons)
 ])
