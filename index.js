8c2c6ae29cdfbad24c3e278a2f793168bf3520b5
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-06-04 17:02:12 +0800

    3.15-3.18: phonebook database, step3 to step6

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ app.use(morgan(
 ))
 
 app.get('/info', (_request, response) => {
-  const phonebookInfo = `Phonebook has info for ${persons.length} people`
-  response.send(`<!DOCTYPE html>
 
-  <div>
-  <p>${phonebookInfo}</p>
-  <p>${new Date()}</p>
-  </div>
-  `)
+  Person.find({})
+  .then(persons => {
+    const phonebookInfo = `Phonebook has info for ${persons.length} people`
+    return response.send(`<!DOCTYPE html>
+    <div>
+    <p>${phonebookInfo}</p>
+    <p>${new Date()}</p>
+    </div>
+    `)
+  })
 })
 
 app.get('/api/persons', (_request, response) => [
@@ -32,18 +35,19 @@ app.get('/api/persons', (_request, response) => [
   Person.find({}).then(persons => response.json(persons))
 ])
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
 
   Person.findById(request.params.id)
   .then(person => {
     if (!person) {
       return response.status(404).json({error: 'could not find person with given id'})
     }
-    response.json(person)
+    return response.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   const body = request.body
   if (!body) {
@@ -56,24 +60,64 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'number not provided' })
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number
+  Person.findOne({ name: body.name })
+  .then(pp => { // Check if the person already exists in the database
+    if (pp) {
+      return fetch(`/api/persons/${pp.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ number: body.number })
+      })
+
+    } else {
+      pp = new person({
+        name: body.name,
+        number: body.number
+      })
+      return pp.save().then(savedperson => response.json(savedperson))
+    }
   })
-  person.save().then(savedPerson => response.json(savedPerson))
+  .then(savedperson => response.json(savedperson))
+  .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+
+  const body = request.body
+  if (!body) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  if (!body.number) {
+    return response.status(400).json({ error: 'number not provided' })
+  }
+
+  Person.findByIdAndUpdate(request.params.id, { number: body.number }, { new: true })
+  .then(savedPerson => response.json(savedPerson))
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
 
   const id = request.params.id
-  Person.findByIdAndDelete(id).then(_ => response.status(204).end())
+  Person.findByIdAndDelete(id)
+  .then(_ => response.status(204).end())
+  .catch(error => next(error))
 })
 
 const unknownEndpoint = (_request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, _request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
